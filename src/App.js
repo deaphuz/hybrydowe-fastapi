44b96531490b525c4d1d8f031535bad7e8f48afb
kgrygo <katarzynagrygo@icloud.com>
2024-04-06 00:10:23 +0200

    Walidacja danych przy dodawaniu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     const [failures, setFailures] = useState([]);
     const [newFailure, setNewFailure] = useState({
         name: '',
+        failureType: '',
         potentialPrice: '',
         potentialDate: '',
         status: 'NEW',
@@ -36,7 +37,31 @@ function App() {
         }));
     };
 
+    const handleStatusChange = (value, index) => {
+
+    };
+
     const addFailure = () => {
+
+        if (newFailure.name.trim().length < 3 || newFailure.failureType.trim().length < 3) {
+            alert("Nazwa urz¹dzenia i rodzaj awarii musz¹ zawieraæ co najmniej 3 znaki.");
+            return;
+        }
+
+        if (isNaN(newFailure.potentialPrice) || newFailure.potentialPrice <= 0) {
+            alert("Szacowany koszt musi byæ liczb¹ wiêksz¹ ni¿ 0.");
+            return;
+        }
+
+        const potentialDate = new Date(newFailure.potentialDate);
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        if (potentialDate <= today) {
+            alert("Szacowana data musi byæ póŸniejsza ni¿ dzisiejsza data.");
+            return;
+        }
+
+
         const newFailureEntry = { ...newFailure, date: new Date().toISOString().slice(0, 10) };
         console.log(newFailureEntry);
         setFailures([...failures, newFailureEntry]);
@@ -48,9 +73,6 @@ function App() {
             repairDescription: ''
         });
     };
-
-
-    
 
 
     return (
@@ -65,8 +87,12 @@ function App() {
                             <input type="text" name="name" value={newFailure.name} onChange={handleInputChange} className="form-control" />
                         </div>
                         <div className="form-group">
+                            <label>Rodzaj awarii:</label>
+                            <input type="text" name="failureType" value={newFailure.failureType} onChange={handleInputChange} className="form-control" />
+                        </div>
+                        <div className="form-group">
                             <label>Szacowany koszt:</label>
-                            <input type="text" name="potentialPrice" value={newFailure.potentialPrice} onChange={handleInputChange} className="form-control" />
+                            <input type="number" name="potentialPrice" value={newFailure.potentialPrice} onChange={handleInputChange} className="form-control" />
                         </div>
                         <div className="form-group">
                             <label>Szacowany termin ukonczenia:</label>
@@ -83,7 +109,7 @@ function App() {
                     <h2>Lista usterek</h2>
                     <ul className="failure-items">
                         {failures.map((failure, index) => (
-                            <Report Failure={failure} Index={index}></Report>
+                            <Report Failure={failure} Index={index} handleStatusChange={handleStatusChange}></Report>
                         ))}
                     </ul>
                 </div>
--- src/Report.js
+++ src/Report.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-function Report({ Failure, Index }) {
+function Report({ Failure, Index, handleStatusChange }) {
     const [failures, setFailures] = useState([]);
     const [editIndex, setEditIndex] = useState(null);
-    const [editedName, setEditedName] = useState('');
-    const [editedDescription, setEditedDescription] = useState('');
+    const [editedFailure, setEditedFailure] = useState({
+        potentialPrice: '',
+        potentialDate: '',
+        repairDescription: ''
+    });
+
+    useEffect(() => {
+        setFailures(Failure);
+        setEditedFailure({
+            potentialPrice: Failure.potentialPrice,
+            potentialDate: Failure.potentialDate,
+            repairDescription: Failure.repairDescription
+        });
+    }, [Failure]);
 
     const handleEdit = (index) => {
         setEditIndex(index);
-        setEditedName(failures[index]?.name || '');
-        setEditedDescription(failures[index].repair_description);
+        setEditedFailure({
+            potentialPrice: Failure.potentialPrice,
+            potentialDate: Failure.potentialDate,
+            repairDescription: Failure.repairDescription
+        });
     };
 
     const handleDelete = (index) => {
@@ -19,35 +34,53 @@ function Report({ Failure, Index }) {
     };
 
     const saveEditedData = (index) => {
-        const updatedFailures = [...failures];
-        updatedFailures[index].name = editedName;
-        updatedFailures[index].repair_description = editedDescription;
-        setFailures(updatedFailures);
+        handleStatusChange(editedFailure, index);
         setEditIndex(null);
-        setEditedName('');
-        setEditedDescription('');
     };
+
 
 	return(
             <li key={Index} className="failure-item">
-                <div className="failure-details">
-                    <p>Rodzaj awarii: {Failure.failureType}</p>
-                    <p>Nazwa urzadzenia: {Failure.name}</p>
-                    <p>Data zgloszenia: {Failure.date}</p>
-                    <p>Szacowany koszt: {Failure.potentialPrice}</p>
-                    <p>Szacowany termin ukonczenia: {Failure.potentialDate}</p>
-                    <p>Status: {Failure.status}</p>
-                    <p>Opis podjetych krokow: {Failure.repairDescription}</p>
-                    {Index === editIndex ? (
+            <div className="failure-details">
+                {Index === editIndex ? (
+                    <div>
+                        <p>Rodzaj awarii: {Failure.failureType}</p>
+                        <p>Nazwa urzadzenia: {Failure.name}</p>
+                        <p>Data zgloszenia: {Failure.date}</p>
+                        <p>Szacowany koszt: <input type="number" placeholder={Failure.potentialPrice} className="form-control"></input> </p>
+                        <p>Szacowany termin ukonczenia: <input type="date" placeholder className="form-control"></input></p>
+                        <div className="form-group">
+                            <label>Status:</label>
+                            <select value={Failure.status} onChange={(e) => handleStatusChange(e, Index)} className="form-control">
+                                <option value="NEW">NEW</option>
+                                <option value="IN_PROGRESS">IN PROGRESS</option>
+                                <option value="FINISHED">FINISHED</option>
+                                <option value="UNREPAIRABLE">UNREPAIRABLE</option>
+                            </select>
+                        </div>
+                        <p>Opis podjetych krokow: <input type="text" placeholder={Failure.repairDescription} className="form-control"></input></p>
+                    
                         <div>
                             <button onClick={() => saveEditedData(Index)} className="btn btn-save">Zapisz</button>
                             <button onClick={() => setEditIndex(null)} className="btn btn-cancel">Anuluj</button>
                         </div>
-                    ) : (
+                    </div>
+                ) : (
+                    <div>
+                        <p>Rodzaj awarii: {Failure.failureType}</p>
+                        <p>Nazwa urzadzenia: {Failure.name}</p>
+                        <p>Data zgloszenia: {Failure.date}</p>
+                        <p>Szacowany koszt: {Failure.potentialPrice}</p>
+                        <p>Szacowany termin ukonczenia: {Failure.potentialDate}</p>
+                        <p>Status: {Failure.status}</p>
+                        <p>Opis podjetych krokow: {Failure.repairDescription}</p>
+
+
                         <div className="failure-actions">
                             <button onClick={() => handleEdit(Index)} className="btn btn-edit">Edytuj</button>
                             <button onClick={() => handleDelete(Index)} className="btn btn-delete">Usun</button>
                         </div>
+                    </div>
                     )}
                 </div>
             </li>
